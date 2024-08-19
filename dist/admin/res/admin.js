efbca8ca8c06ac15b5693ba6c0529bfa1185111d
jackb0back <54455407+jackb0back@users.noreply.github.com>
2024-08-18 21:51:26 -0400

    add basic peer

--- dist/admin/res/admin.js
+++ dist/admin/res/admin.js
@@ -1,18 +1,41 @@
-const socket = io("/admin",{
+const socket = io("/admin", {
     auth: {
         token: "2f35c1073c86b7087a504ad9183f6a24ced9f49a7b29f5aac60acccadd62cafb"
     }
 });
 
+var peer = new Peer();
+var currentConnection;
 
+function connect(id) {
+    var conn = peer.connect(id);
+    currentConnection = conn;
+    conn.on('open', function() {
+        console.log("connected to peer:", id);
+        // Receive messages
+        conn.on('data', function(data) {
+            console.log('Received', data);
+        });
+
+        // Send messages
+        conn.send('Hello!');
+    });
+
+    conn.on('error', function(err) {
+        console.error('Connection error:', err);
+    });
+}
 
 socket.on('connect', () => {
-    console.log("connected");
+    console.log("socket connected");
     setTimeout(() => {
-        socket.emit("getHooks","");
-    })
+        socket.emit("getHooks", "");
+    });
 });
 
 socket.on("hooks", (data) => {
     console.log(data);
-})
+    if (data && data.length > 0) {
+        connect(data[0].peer); // Assuming data contains peer IDs
+    }
+});
--- dist/hook/hook.js
+++ dist/hook/hook.js
@@ -1,32 +1,53 @@
 var peer;
+var connections = [];
+
 if (localStorage.getItem("_peer") !== null) {
     peer = new Peer(localStorage.getItem("_peer"));
-}else {
+} else {
     peer = new Peer(generateRandomString(10));
-    localStorage.setItem("_peer",peer.id);
+    localStorage.setItem("_peer", peer.id);
 }
 
-//Websocket
 const socket = io("/hook");
 socket.on('connect', () => {
-    console.log("connected");
-    setTimeout(function(){
-        socket.emit("newHook",{
+    console.log("socket connected");
+    setTimeout(function() {
+        socket.emit("newHook", {
             "peer": peer.id,
             "page": window.location.href,
             "title": document.title
         });
-    },150)
+    }, 150);
 });
 
+peer.on('open', function(id) {
+    console.log('My peer ID is: ' + id);
+});
 
+peer.on('connection', (conn) => {
+    console.log("Incoming connection from peer:", conn.peer);
+    connections.push(conn);
+
+    conn.on('open', () => {
+        console.log("Connected to peer:", conn.peer);
+        conn.send("Hello from peer!");
+    });
+
+    conn.on('data', (data) => {
+        console.log("Received data from peer:", data);
+    });
+
+    conn.on('error', (err) => {
+        console.error('Connection error:', err);
+    });
+});
 
 function generateRandomString(length) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
     const charactersLength = characters.length;
     for (let i = 0; i < length; i++) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
 }
